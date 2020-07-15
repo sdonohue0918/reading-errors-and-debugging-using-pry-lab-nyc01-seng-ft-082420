f53558ae9fe23cd16e29e745d3c060e8668f9c50
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-15 21:36:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,10 +2,12 @@
 
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
-  binding.pry
+
 end
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
-  binding.pry
+
 end
+
+start_game("Ferry", "Dingus")
